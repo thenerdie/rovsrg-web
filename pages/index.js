0e30864de0a38ac0be38adb56f1f003cb5ef18c1
astralkingdummy <cjansah11@gmail.com>
2022-01-15 12:57:57 -0500

    provide noblox and env

--- pages/index.js
+++ pages/index.js
@@ -4,6 +4,8 @@ import styles from '../styles/Home.module.css'
 
 import Game from "./components/home/game"
 
+const noblox = require('noblox.js');
+
 export default function Home() {
   return (
     <div className="flex flex-col">
